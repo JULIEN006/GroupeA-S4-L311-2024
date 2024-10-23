1561b6eab36fa70f16785a7690e6545cb47a378b
samenthalo <ortega.samantha81400@gmail.com>
2024-10-23 17:02:01 +0200

    Modification du nom du fichier

---
+++ inc/inc.style.php
@@ -0,0 +1,7 @@
+<!-- Lien vers la feuille de style principale -->
+<link rel="stylesheet" href="assets/css/main.css" />
+
+<!-- Feuille de style alternative pour les navigateurs n'acceptant pas JavaScript -->
+<noscript>
+    <link rel="stylesheet" href="assets/css/noscript.css" />
+</noscript>
