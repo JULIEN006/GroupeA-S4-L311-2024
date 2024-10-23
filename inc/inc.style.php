539e214659c1a233893a536315e1cad426b0051e
samenthalo <124697184+samenthalo@users.noreply.github.com>
2024-10-23 17:34:52 +0200

    Delete inc/inc.style.php

--- inc/inc.style.php
+++
@@ -1,7 +0,0 @@
-<!-- Lien vers la feuille de style principale -->
-<link rel="stylesheet" href="assets/css/main.css" />
-
-<!-- Feuille de style alternative pour les navigateurs n'acceptant pas JavaScript -->
-<noscript>
-    <link rel="stylesheet" href="assets/css/noscript.css" />
-</noscript>
