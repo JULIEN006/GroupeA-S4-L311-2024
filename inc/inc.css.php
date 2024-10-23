518c7e04e626cd74caca950b029d304040f6fde4
hugolgs-dev <hugo.ligneres@orange.fr>
2024-10-23 17:53:14 +0200

    suppr inc.css.php & inc.js.php

--- inc/inc.css.php
+++
@@ -1,6 +0,0 @@
-<?php
-//test2
-ini_set('display_errors', 'On');
-error_reporting(E_ALL | E_STRICT);?>
-<link rel="stylesheet" href="assets/css/main.css" />
-<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
