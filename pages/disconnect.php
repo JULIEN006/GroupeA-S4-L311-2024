1e8a72af549c3074afe39a8dab8dc3937ac71313
hugolgs-dev <hugo.ligneres@orange.fr>
2024-10-25 16:26:01 +0200

    fix disconnect

--- pages/disconnect.php
+++ pages/disconnect.php
@@ -1,8 +1,12 @@
 <?php
+//Affichage des erreurs de debogage
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);?>
 <?php
-	setDisconnectUser();
+    include "../inc/inc.functions.php";
+
+
+    setDisconnectUser();
 
 	header('Location:index.php');
 ?>
